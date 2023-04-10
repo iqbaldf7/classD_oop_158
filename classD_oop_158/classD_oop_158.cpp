aa1ecea451bf28599451aeeda43653dcd909ef16
iqbaldf7 <ACER@LAPTOP-BIGAQTOQ>
2023-04-10 19:46:18 +0700

    Add project files.

---
+++ classD_oop_158/classD_oop_158.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+class Mahasiswa {
+public:
+	string nim;
+	string nama;
+public: void cetak() {
+	cout << "NIM = " << nim;
+	cout << "\nNama = " << nama;
+}
+};
+
+class Matakuliah {
+private:
+	string kode;
+	string namaM;
+	int sks;
+public:
+	void input() {
+		cout << "Masukan Kode Matakuliah : ";
+		cin >> kode;
+		cout << "Masukan Nama Matakuliah : ";
+		cin >> namaM;
+		cout << "Jumlah SKS : ";
+		cin >> sks;
+	}
+	void tampil() {
+		cout << "Kode Matakuliah : " << kode;
+		cout << "Nama Matakuliah : " << namaM;
+		cout << "SKS : " << sks;
+	}
+};
+
+int main()
+{
+	Mahasiswa mhs1;
+	Matakuliah mk;
+
+	cout << "Masukan NIM : ";
+	cin >> mhs1.nim;
+	cout << "Masukan nama : ";
+	cin >> mhs1.nama;
+	mhs1.cetak();
+
+	mk.input();
+	mk.tampil();
+}
